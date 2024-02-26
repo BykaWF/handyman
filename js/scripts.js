5ccd070bc05e4c03867605cba2620fd9799a084e
BykaWF <yaroslavprozorov213@gmail.com>
2024-02-26 16:55:07 +0100

    Smoth with JS

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+// Define an array of words to rotate
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Array of words to rotate
+    var words = ["SIMPLE", "EASY", "FLEXIBLE"];
+    var rotatingHeading = document.getElementById("rotatingHeading");
+    var currentIndex = 0;
+
+    setInterval(function() {
+      rotatingHeading.textContent = "MAKE IT " + words[currentIndex];
+      currentIndex = (currentIndex + 1) % words.length;
+    }, 5000); // Rotate every 5 seconds
+  });
